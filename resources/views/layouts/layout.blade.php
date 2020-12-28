604aadf0068c3f1e26430251839d96f2a7503360
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-27 21:24:34 -0400

    Layout creado
    Se creó el layout o plantilla base para el proyecto

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Tips - @yield('title')</title>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            {{-- Botón de controles en el responsive --}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {{-- Alineación botón a la izquierda logo a la derecha --}}
+            <a href="{{ route('commons.home') }}" class="navbar-brand">MHenriquez</a>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('commons.home') }}">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('commons.about') }}">Nosotros</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
