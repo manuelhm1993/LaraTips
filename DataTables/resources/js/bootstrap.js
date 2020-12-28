b987027f911e560e70fb4ea30cdfc95db672af89
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-28 18:44:39 -0400

    Instalación de datatables
    Luego de instalar las dependencias con npm se deben importar en el archivo resources/js/bootstrap.js y posteriormente compilarlas

--- DataTables/resources/js/bootstrap.js
+++ DataTables/resources/js/bootstrap.js
@@ -24,6 +24,11 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Importar las datatables
+ */
+window.dt = require( 'datatables.net' )();
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
--- DataTables/resources/views/index.blade.php
+++ DataTables/resources/views/index.blade.php
@@ -5,10 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>DataTables</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div class="container">
-            
+            <h1>Hola mundo</h1>
         </div>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
