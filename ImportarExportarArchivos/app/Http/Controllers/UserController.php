d53016a83757efc7dfb026e059d2700d9cb0338a
Manuel Henriquez <manuelhm1993@gmail.com>
2020-12-31 00:36:14 -0400

    Método para descargar PDF
    Se completó el método para extraer todos los usuarios de la BBDD y descargarlos en un PDF

--- ImportarExportarArchivos/app/Http/Controllers/UserController.php
+++ ImportarExportarArchivos/app/Http/Controllers/UserController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Barryvdh\DomPDF\Facade as PDF;//Clase que permite usar las herramientas PDF
+
+use App\User;
+
 class UserController extends Controller
 {
     public function exportPDF()
     {
-        return 'llegué';
+        $users = User::all();
+        $pdf   = PDF::loadView('pdf.users', compact('users'));
+
+        //El parámetro que recibe es el nombre que tendrá el documento
+        return $pdf->download('user-list.pdf');
     }
 }
